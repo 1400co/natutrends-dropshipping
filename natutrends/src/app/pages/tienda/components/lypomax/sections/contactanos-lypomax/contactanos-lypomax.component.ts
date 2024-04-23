8d2fd90508cb9b29e8552400099a1e65203c68b7
Oscar Alonso Rueda Ruiz <138699802+oruedarr@users.noreply.github.com>
2024-04-22 20:37:20 -0500

    cambios para seguimiento en fb

--- natutrends/src/app/pages/tienda/components/lypomax/sections/contactanos-lypomax/contactanos-lypomax.component.ts
+++ natutrends/src/app/pages/tienda/components/lypomax/sections/contactanos-lypomax/contactanos-lypomax.component.ts
@@ -1,10 +1,39 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { PixelService } from 'ngx-multi-pixel';
 
 @Component({
   selector: 'app-contactanos-lypomax',
   templateUrl: './contactanos-lypomax.component.html',
   styleUrl: './contactanos-lypomax.component.scss'
 })
-export class ContactanosLypomaxComponent {
+export class ContactanosLypomaxComponent implements OnInit {
   @Input() phoneNumber:string = '';
+  fbId:string = '';
+
+  constructor(private pixel: PixelService,
+    private activatedRoute: ActivatedRoute,)
+  {
+
+  }
+
+  ngOnInit():void{
+    this.phoneNumber =  this.activatedRoute.snapshot.params['phoneNumber']; //get from route snapshot
+    this.fbId =  this.activatedRoute.snapshot.params['facebookId']; //get from route snapshot
+    this.onConsent();
+  }
+
+  onConsent(): void {
+    this.pixel.initialize(['1676952112730164']);
+  }
+
+  onContact() {
+    this.pixel.track("Contact", {});
+    if (this.phoneNumber) {
+      const whatsappURL = `https://wa.me/${this.phoneNumber}`;
+      window.open(whatsappURL, '_blank');
+    } else {
+      console.error('Número de teléfono no proporcionado');
+    }
+  }
 }
--- natutrends/src/app/pages/tienda/components/lypomax/sections/kit-lypomax/kit-lypomax.component.ts
+++ natutrends/src/app/pages/tienda/components/lypomax/sections/kit-lypomax/kit-lypomax.component.ts
@@ -1,4 +1,6 @@
 import { Component, Input } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { PixelService } from 'ngx-multi-pixel';
 
 @Component({
   selector: 'app-kit-lypomax',
@@ -7,4 +9,31 @@ import { Component, Input } from '@angular/core';
 })
 export class KitLypomaxComponent {
   @Input() phoneNumber:string = '';
+  fbId:string = '';
+
+  constructor(private pixel: PixelService,
+    private activatedRoute: ActivatedRoute,)
+  {
+
+  }
+
+  ngOnInit():void{
+    this.phoneNumber =  this.activatedRoute.snapshot.params['phoneNumber']; //get from route snapshot
+    this.fbId =  this.activatedRoute.snapshot.params['facebookId']; //get from route snapshot
+    this.onConsent();
+  }
+
+  onConsent(): void {
+    this.pixel.initialize(['1676952112730164']);
+  }
+
+  onContact() {
+    this.pixel.track("Contact", {});
+    if (this.phoneNumber) {
+      const whatsappURL = `https://wa.me/${this.phoneNumber}`;
+      window.open(whatsappURL, '_blank');
+    } else {
+      console.error('Número de teléfono no proporcionado');
+    }
+  }
 }
