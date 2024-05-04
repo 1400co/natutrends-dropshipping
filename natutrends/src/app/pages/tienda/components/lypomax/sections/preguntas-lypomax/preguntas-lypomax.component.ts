e855ace8c49002ff269e5319ac47daaba86dca7c
Oscar Alonso Rueda Ruiz <138699802+oruedarr@users.noreply.github.com>
2024-05-03 19:37:44 -0500

    cambios para vender

--- natutrends/src/app/pages/tienda/components/lypomax/sections/preguntas-lypomax/preguntas-lypomax.component.ts
+++ natutrends/src/app/pages/tienda/components/lypomax/sections/preguntas-lypomax/preguntas-lypomax.component.ts
@@ -1,9 +1,10 @@
 import { HttpClient } from '@angular/common/http';
-import { Component, Input } from '@angular/core';
+import { Component, Input, ViewChild } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { ActivatedRoute } from '@angular/router';
 import { CookieService } from 'ngx-cookie-service';
 import { PixelService } from 'ngx-multi-pixel';
+
 
 @Component({
   selector: 'app-preguntas-lypomax',
@@ -12,16 +13,17 @@ import { PixelService } from 'ngx-multi-pixel';
 })
 export class PreguntasLypomaxComponent {
 
+  showModal = false;
+
   pedidoForm: FormGroup = new FormGroup({});
-  @Input() phoneNumber:string = '';
-  fbId:string = '';
+  @Input() phoneNumber: string = '';
+  fbId: string = '';
 
   constructor(private fb: FormBuilder,
     private activatedRoute: ActivatedRoute,
     private pixel: PixelService,
     private cookieService: CookieService,
-    private http: HttpClient)
-    { }
+    private http: HttpClient) { }
 
   ngOnInit(): void {
     this.pedidoForm = this.fb.group({
@@ -33,8 +35,8 @@ export class PreguntasLypomaxComponent {
       productos: ['', Validators.required]
     });
 
-    this.phoneNumber =  this.activatedRoute.snapshot.params['phoneNumber']; //get from route snapshot
-    this.fbId =  this.activatedRoute.snapshot.params['facebookId']; //get from route snapshot
+    this.phoneNumber = this.activatedRoute.snapshot.params['phoneNumber']; //get from route snapshot
+    this.fbId = this.activatedRoute.snapshot.params['facebookId']; //get from route snapshot
     this.onConsent();
     this.loadFormDataFromCookie();
   }
@@ -47,45 +49,45 @@ export class PreguntasLypomaxComponent {
   }
 
   onConsent(): void {
-    this.pixel.initialize(['1676952112730164',this.fbId ]);
+    this.pixel.initialize(['1676952112730164', this.fbId]);
   }
 
   submitPedido() {
     if (this.pedidoForm.valid) {
       const formData = this.pedidoForm.value;
-      debugger
+
       this.cookieService.set('pedidoNatutrendsFormData', JSON.stringify(formData));
 
-      // Construir el mensaje legible para enviar al API de WhatsApp
-      const mensajeWhatsapp = `Hola! Te contactamos para realizar un pedido:\n\n` +
-        `**Nombre Completo:** ${formData.nombreCompleto}\n` +
-        `**Número de Celular:** ${formData.numeroCelular}\n` +
-        `**Departamento:** ${formData.departamento}\n` +
-        `**Ciudad:** ${formData.ciudad}\n` +
-        `**Direccion:** ${formData.direccion}\n` +
-        `**Productos:** ${formData.productos}`;
+      // Build the message to send through the new API
+      const mensaje = {
+        number: this.phoneNumber, // Destination number
+        message: `Hola! Te contactamos para realizar un pedido:\n\n` +
+          `**Nombre Completo:** ${formData.nombreCompleto}\n` +
+          `**Número de Celular:** ${formData.numeroCelular}\n` +
+          `**Departamento:** ${formData.departamento}\n` +
+          `**Ciudad:** ${formData.ciudad}\n` +
+          `**Direccion:** ${formData.direccion}\n` +
+          `**Productos:** ${formData.productos}`,
+        urlMedia: null // Assuming there is no media to send by default
+      };
 
-      // Realizar la solicitud POST al API de WhatsApp
-      this.http.post('https://whatsapp.nuestrapeople.com/v1/messages', {
-        number: this.phoneNumber, // Número de destino
-        message: mensajeWhatsapp // Mensaje a enviar
-      }).subscribe(
+      // Send the POST request to the new API endpoint
+      this.http.post('https://whatsapp.nuestrapeople.com/v1/messages', mensaje, { responseType: 'text' }).subscribe(
         response => {
-          // Manejar la respuesta del servidor si es necesario
+          console.log('Server response:', response);
           this.onCheckout();
-          alert('Su pedido ha sido registrado con exito.')
-          console.log('Respuesta del servidor:', response);
+          this.showModal = true; // Open modal on successful submission
         },
         error => {
-          // Manejar errores si la solicitud falla
-          alert('Ha ocurrido un error intentelo mas tarde.')
-          console.error('Error al enviar la solicitud:', error);
+          console.error('Error sending the request:', error);
+          alert('Ha ocurrido un error, inténtelo más tarde.');
         }
       );
     } else {
       this.pedidoForm.markAllAsTouched();
     }
   }
+
 
   submitPedido2() {
     if (this.pedidoForm.valid) {
@@ -103,7 +105,7 @@ export class PreguntasLypomaxComponent {
 
       // Build the WhatsApp URL
       const whatsappURL = `https://wa.me/${this.phoneNumber}?text=${mensajeWhatsapp}`;
-      
+
       this.onCheckout();
       // Redirect to the WhatsApp URL
       window.open(whatsappURL, '_blank');
@@ -114,14 +116,15 @@ export class PreguntasLypomaxComponent {
 
 
 
-  onCheckout()
-  {
+  onCheckout() {
     this.pixel.track("Purchase", {
       content_ids: ["Lypomax", "L-01"], // Item SKUs
       value: 69000, // Value of all items
       currency: "COP", // Currency of the value
     });
   }
+
+
 
 
 
