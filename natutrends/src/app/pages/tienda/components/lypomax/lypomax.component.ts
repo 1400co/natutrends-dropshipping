7c791863feacfedb6846bc3bcd6e9981b081c1b4
Oscar Alonso Rueda Ruiz <138699802+oruedarr@users.noreply.github.com>
2024-05-04 18:59:12 -0500

    titulo y emoticones

--- natutrends/src/app/pages/tienda/components/lypomax/lypomax.component.ts
+++ natutrends/src/app/pages/tienda/components/lypomax/lypomax.component.ts
@@ -1,6 +1,8 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { PixelService } from 'ngx-multi-pixel';
+import { Title, Meta  } from '@angular/platform-browser';
+
 
 @Component({
   selector: 'app-lypomax',
@@ -14,11 +16,14 @@ export class LypomaxComponent implements OnInit {
   fbId:string = '';
 
   constructor(private activatedRoute: ActivatedRoute,
-    private pixel: PixelService
+    private pixel: PixelService,
+    private metaService: Meta,
+    private titleService: Title
     )
-  {
-
-  }
+      {
+        this.titleService.setTitle('Lypomax - Natutrends');
+        this.metaService.updateTag({ name: 'description', content: 'Este batido sustituto de cena combina la robusta bondad de los corazones de alcachofa 🍃 con el poder antiinflamatorio de la cúrcuma 🌼 y las propiedades desintoxicantes del rábano 🍽️. Ideal para la pérdida de peso, apoya la salud hepática y la digestión gracias a los corazones de alcachofa, mientras que la cúrcuma aporta antioxidantes que pueden reducir la inflamación y mejorar la salud articular. Los rábanos ayudan a purificar la sangre y eliminar toxinas, haciendo de este batido un final nutritivo para tu día, prometiendo bienestar y manejo del peso.' });
+      }
 
   ngOnInit():void{
     this.phoneNumber =  this.activatedRoute.snapshot.params['phoneNumber']; //get from route snapshot
